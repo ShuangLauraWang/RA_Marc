481235ad2dc8fe89ed1e20e20e80f364b065ddd8
[Shuang Wang] <[laura.wang2009@gmail.com]>
2018-09-26 16:12:18 -0400

    mean.RData: output saved

--- Code/data_clean.R
+++ Code/data_clean.R
@@ -112,61 +112,16 @@ obj.fn <- function(params, obs){
         
         q.true <- qlnorm(p, mu, sigma)
         
-        weights <- sqrt(p * (1 - p) / dlnorm(q.true)^2)
+        #weights <- sqrt(p * (1 - p) / dlnorm(q.true)^2)
 
-        #weights <- rep(1, length(p))
-        diff <- c(((q.obs - q.true)/weights)[!is.na(q.obs)], 
-                  ((mean.obs - mean.true)/weights)[!is.na(mean.obs)])
+        diff <- c(((q.obs - q.true)/q.obs)[!is.na(q.obs)], 
+                  ((mean.obs - mean.true)/mean.obs)[!is.na(mean.obs)])
 
         obj.value <- sum(diff^2)
         
         return(obj.value)
 }
 
-optim.fit.constr <- function(x){
-        
-        # This function distinguishes two cases:
-        # (1) 2 or less quantiles are observed, bad empirical fits, return NA's
-        # (2) more than 2 quantiles are observed, estimate with auglag(), which 
-        # allows inequality constraints.
-        # 
-        # args: (1)x: each row of data
-        #       
-        # output: optimal logmean and sdmeand of a log normal distribution
-        
-        lb <- max(which(x == "2,500-"))
-        ub <- min(which(x == "250,000+"))
-        
-        x <- as.numeric(x)
-        
-        if (lb >= 3 | ub <= 3 | sum(!is.na(x)) < 2)
-                return(c(NA, NA))
-        else{
-                
-                hin <- function(params, q){
-                        # inequality constrainsts
-                        
-                        mu <- params[1]
-                        sigma <- params[2]
-                        
-                        h <- rep(NA,1)
-                        
-                        i <- 1
-                        
-                        if (lb > -Inf){
-                                h[i] <- 2500 - qlnorm(p[lb], mu, sigma)
-                                i <- i + 1
-                        }
-                        if (ub < Inf)        
-                                h[i] <- qlnorm(p[ub], mu, sigma) - 250000
-                }
-                
-                return(auglag(par = c(0, 1), 
-                              fn = obj.fn, hin = hin, q = x, 
-                              control.outer = list(trace = F))$par)
-                
-        }
-}
 
 
 optim.fit.constr <- function(x){
@@ -220,7 +175,7 @@ p <- c(seq(from = 0.2, to = 0.8, by = 0.2), 0.95)
 
 
 
-output.constr <- apply(data[1, 4 : 13], 1, FUN = optim.fit.constr)
+output.constr <- apply(data[, 4 : 13], 1, FUN = optim.fit.constr)
 
 # output summary
 apply(output.constr, 1, summary)

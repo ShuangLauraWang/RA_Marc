932d8451b96fb7e13cfc0db7101c65a77e757b9e
[Shuang Wang] <[laura.wang2009@gmail.com]>
2018-10-01 13:16:20 -0400

    data_clean.R: (1)hin deleted; (2)option "weight" added to the obj.fn; (3)local optim existis, start value set at (10, 1)

--- Code/data_clean.R
+++ Code/data_clean.R
@@ -58,8 +58,9 @@ data <- merge(data.temp, data.population,
               by = c("GEO_id", "GEO_id2", "Geography"),
               all.x = T)
 
-#delete tracts without observed quantiles (84 tracts deleted)
-data<- data[apply(data, 1, FUN = function(x) sum(x %in% c("-", "**")) < 10), ] 
+#delete tracts without observed quantiles (162 tracts deleted)
+missing <- apply(data, 1, FUN = function(x) sum(x %in% c("-", "**", "***", NA)))
+data<- data[missing < 12, ] 
 
 row.names(data) <- 1 : nrow(data)
 
@@ -88,7 +89,7 @@ cond.ex <- function(params, lc = 0, uc = Inf){
                (pnorm((log(uc) - mu)/sigma) - pnorm((log(lc) - mu)/sigma))
 }
 
-obj.fn <- function(params, obs){
+obj.fn <- function(params, x, weight){
         # This function is the objective function for log normal distribution fit
         # 
         # args: (1)params: meanlog & sdlog of a log normal
@@ -100,20 +101,24 @@ obj.fn <- function(params, obs){
         mu <- params[1]
         sigma <- params[2]
         
-        q.obs <- obs[1 : 5]
-        mean.obs <- obs[6 : 10]
+        obs <- x[seq(from = 1, by = 2, length.out = 11)]
+        moe <- x[seq(from = 2, by = 2, length.out = 11)]
+        n <- x[length(x)]
 
-        lc <- qlnorm(c(0, p[1 : 4]), mu, sigma)
-        uc <- qlnorm(c(p[1 : 4], 1), mu, sigma)
+        lc <- qlnorm(c(0, p), mu, sigma)
+        uc <- qlnorm(c(p, 1), mu, sigma)
         mean.true <- mapply(cond.ex, lc = lc, uc = uc, MoreArgs = list(params = params))
         
         q.true <- qlnorm(p, mu, sigma)
         
-        #weights <- sqrt(p * (1 - p) / dlnorm(q.true)^2)
+        true <- c(q.true, mean.true)
+        
+        true <- true[!is.na(obs)]
+        moe <- moe[!is.na(obs)]
+        obs <- obs[!is.na(obs)]
+        
 
-        diff <- c(((q.obs - q.true)/q.obs)[!is.na(q.obs)], 
-                  ((mean.obs - mean.true)/mean.obs)[!is.na(mean.obs)])
-
+        diff <- (obs - true)/(moe/sum(moe))^weight
         obj.value <- sum(diff^2)
         
         return(obj.value)
@@ -121,50 +126,6 @@ obj.fn <- function(params, obs){
 
 
 
-optim.fit.constr <- function(x){
-        
-        # This function distinguishes two cases:
-        # (1) 2 or less quantiles are observed, bad empirical fits, return NA's
-        # (2) more than 2 quantiles are observed, estimate with auglag(), which 
-        # allows inequality constraints.
-        # 
-        # args: (1)x: each row of data
-        #       
-        # output: optimal logmean and sdmeand of a log normal distribution
-        
-        q.obs <- x[1 : 5]
-        
-        lb <- max(which(q.obs == "2,500-"))
-        ub <- min(which(q.obs == "250,000+"))
-        
-        x <- as.numeric(x)
-        
-        if (sum(!is.na(x)) < 2)
-                return(c(NA, NA))
-        else{
-                hin <- function(params, obs){
-                                # inequality constrainsts
-                                
-                                mu <- params[1]
-                                sigma <- params[2]
-                                
-                                h <- rep(NA,1)
-                                
-                                i <- 1
-                                
-                                if (lb > -Inf){
-                                        h[i] <- 2500 - qlnorm(p[lb], mu, sigma)
-                                        i <- i + 1
-                                }
-                                if (ub < Inf)        
-                                        h[i] <- qlnorm(p[ub], mu, sigma) - 250000
-                        }
-                        
-                return(auglag(par = c(0, 1), fn = obj.fn, hin = hin, obs = x, 
-                              control.outer = list(trace = F))$par)
-        }            
-        
-}
 
 
 # cut values for quantiles
@@ -172,7 +133,12 @@ p <- c(seq(from = 0.2, to = 0.8, by = 0.2), 0.95)
 
 
 
-output.constr <- apply(data[, 4 : 13], 1, FUN = optim.fit.constr)
+output.constr <- apply(data[, -(1 : 3)], 1, 
+                       FUN = function(x){
+                               x <- as.numeric(x)
+                               optim(par = c(10, 1), fn = obj.fn, x = x, 
+                                     weight = F)$par
+                       })
 
 # output summary
 apply(output.constr, 1, summary)
